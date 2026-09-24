c40073bb862e1ad98adcfb5bf034108a13f42ab5
InfoTpopentconex <tpopentconex@gmail.com>
2026-09-24 19:12:18 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.KOL_IDS_CONFIG = {
-  SUPABASE_URL: 'https://xdmtuoyeyhkiwiozaut.supabase.co',
+  SUPABASE_URL: 'https://xdmtuoyeiyhkwiwozaut.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_FVjCDqfF1JO6uH_Z8PwEeQ__iNaA0Sh',
   ORDER_FORM_URL: 'https://docs.google.com/forms/d/e/1FAIpQLScv-WC7h4CiyZ0EPm-_uTf-fhTSjUR5nXC9mC9cvWt0XknoNA/viewform',
   TRIAL_URL: 'https://docs.google.com/forms/d/e/1FAIpQLScv-WC7h4CiyZ0EPm-_uTf-fhTSjUR5nXC9mC9cvWt0XknoNA/viewform',
