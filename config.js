ca49b93781968d2d6cb83f1a38b2a3e4ad2243bf
InfoTpopentconex <tpopentconex@gmail.com>
2026-09-24 16:33:15 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ window.KOL_IDS_CONFIG = {
   SUPABASE_ANON_KEY: 'sb_publishable_FVjCDqfF1JO6uH_Z8PwEeQ__iNaA0Sh',
   ORDER_FORM_URL: 'https://docs.google.com/forms/d/e/1FAIpQLScv-WC7h4CiyZ0EPm-_uTf-fhTSjUR5nXC9mC9cvWt0XknoNA/viewform',
   TRIAL_URL: 'https://docs.google.com/forms/d/e/1FAIpQLScv-WC7h4CiyZ0EPm-_uTf-fhTSjUR5nXC9mC9cvWt0XknoNA/viewform',
-  PAYMENT_FORM_URL: ''
+  PAYMENT_FORM_URL: '',
+  CONTACT_WEBAPP_URL: 'https://script.google.com/macros/s/AKfycby0ScBhEOtkCJZv5vPC9IiEp78h17JtrG16fab6scqiaRadjGRhLKkCa_-IbUEBRRMK/exec'
 };
